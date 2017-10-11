08c6b0eb5fa610ffc8f41b638c704b3c88b495f4
Salmane <salmane.hajouji@me.com>
2017-10-11 11:15:57 +0200

    refactor: popular

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,39 +1,59 @@
 var React = require('react');
 require('../index.css');
 
-class Popular extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedLanguage: 'All'
-    };
+var PropTypes = require('prop-types');
 
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
-  updateLanguage(lang){
-    this.setState(function(){
-      return {
-        selectedLanguage: lang
-      }
-    });
-  }
-  render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-    return (
-      <ul className='languages'>
-        {languages.map(function(lang){
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? { color : '#d0021b' } : null }
-              onClick={this.updateLanguage.bind(null, lang)}
-              key={lang}>
-              {lang}
-            </li>
-          )
-        }, this)}
-      </ul>
-    )
-  }
+function SelectLanguage(props) {
+	var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+	return (
+		<ul className="languages">
+			{languages.map(function(lang) {
+				return (
+					<li
+						style={
+							lang === props.selectedLanguage ? { color: '#d0021b' } : null
+						}
+						onClick={props.onSelect.bind(null, lang)}
+						key={lang}
+					>
+						{lang}
+					</li>
+				);
+			}, this)}
+		</ul>
+	);
 }
+
+class Popular extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedLanguage: 'All'
+		};
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+	updateLanguage(lang) {
+		this.setState(function() {
+			return {
+				selectedLanguage: lang
+			};
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SelectLanguage
+					selectedLanguage={this.state.selectedLanguage}
+					onSelect={this.updateLanguage}
+				/>
+			</div>
+		);
+	}
+}
+
+SelectLanguage.propTypes = {
+	selectedLanguage: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired
+};
 
 module.exports = Popular;
